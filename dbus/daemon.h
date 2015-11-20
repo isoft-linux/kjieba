670446f030e301e0f25fd95abf2fbf6a27f60122
Leslie Zhai <xiangzhai83@gmail.com>
2015-11-20 16:56:33 +0800

    Add dbus

---
+++ dbus/daemon.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 Leslie Zhai <xiang.zhai@i-soft.com.cn>
+ * Copyright 2014 Marco Martin <mart@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef DAEMON_H
+#define DAEMON_H
+
+class Daemon : public QObject
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.isoftlinux.kjieba.App")
+
+public:
+    explicit Daemon(QObject *parent = 0);
+    virtual ~Daemon();
+
+public Q_SLOTS:
+    void query(const QString &term);
+};
+
+#endif // DAEMON_H
---
+++ dbus/main.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2015 Leslie Zhai <xiang.zhai@i-soft.com.cn>
+ * Copyright 2012 Marco Martin <mart@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <QApplication>
+#include <KLocalizedString>
+
+#include <qcommandlineparser.h>
+#include <qcommandlineoption.h>
+#include <QDebug>
+#include <QSessionManager>
+
+#include <KAboutData>
+#include <kdbusservice.h>
+
+#include "app.h"
+
+static QCommandLineParser parser;
+
+int main(int argc, char *argv[])
+{
+    KLocalizedString::setApplicationDomain("kjieba");
+
+    QApplication app(argc, argv);
+    app.setApplicationName("kjieba");
+    app.setOrganizationDomain("isoftlinux.org");
+    app.setApplicationVersion(PROJECT_VERSION);
+    app.setQuitOnLastWindowClosed(false);
+    parser.setApplicationDescription(i18n("libcppjieba interface"));
+    KDBusService service(KDBusService::Unique);
+
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.process(app);
+
+    KAboutData aboutData("kjieba",
+        i18n("kjieba"),
+        PROJECT_VERSION,
+        i18n("libcppjieba interface"),
+        KAboutLicense::GPL);
+
+    KAboutData::setApplicationData(aboutData);
+
+    auto disableSessionManagement = [](QSessionManager &sm) {
+        sm.setRestartHint(QSessionManager::RestartNever);
+    };
+    QObject::connect(&app, &QGuiApplication::commitDataRequest, disableSessionManagement);
+    QObject::connect(&app, &QGuiApplication::saveStateRequest, disableSessionManagement);
+
+    Daemon daemon;
+
+    QObject::connect(&service, &KDBusService::activateRequested, &daemon, &Daemon::foo);
+
+    return app.exec();
+}
